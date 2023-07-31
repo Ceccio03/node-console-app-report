0e59b19b3341772cb3c780cfc520dbdfa8d66554
Ceccio03 <76108781+Ceccio03@users.noreply.github.com>
2023-07-31 13:18:24 +0200

    new repo

---
+++ app.js
@@ -0,0 +1,29 @@
+const fs = require("fs");
+const inputUrl = process.argv[2];
+const outputUrl = process.argv[3];
+let data = readFile(inputUrl);
+
+if (data) {
+
+    const result = transformFunction(data, divider)
+
+    writeData(outputUrl, result);
+}
+
+function readFile(url){
+    try {
+        const data = fs.readFileSync(url, "utf8");
+        return data;
+    } catch (err) {
+        console.error(err.message);
+        return null;
+    }
+}
+
+function writeData(url, data){
+    try {
+        fs.writeFileSync(url, data);
+    } catch (err) {
+        console.error(err.message);
+    }
+}
